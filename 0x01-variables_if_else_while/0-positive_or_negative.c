fd870f1f18a9aaa520a85670ce8c99fb9708276c
Adeyemo victor <adeyemovictoroyinlola@gmail.com>
2023-12-15 10:20:31 -0800

    aaaaaaa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Generate a random number and determine if it
+ * is positive, negative, or zero
+ *
+ * This function generates a random number using the
+ * rand() function seeded with the current time.
+ * It then checks whether the number is positive,
+ * negative, or zero, and outputs the corresponding
+ * string value to the console.
  *
  * Return: Always 0.
-*/
+ */
+
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,42 @@
-/*
- * File: 2-print_alphabet.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Description: The main function generates a random 
+ * number and
+ * computes the last digit to output the corresponding 
+ * string.
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	char letter;
+	int n;
+	int lastdigit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	lastdigit = n % 10;
 
-	putchar('\n');
-
+	if (lastdigit > 5)
+	{
+	printf("Last digit of %d is %d and is greater 
+			than 5\n", n, lastdigit);
+	}
+	else if (lastdigit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, 
+			lastdigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less 
+			than 6 and not 0\n", n, 
+			lastdigit);
+	}
 	return (0);
-}
+	}
