2d3525c66b07ae7d043df5608993e09cbc5e172c
Adeyemo victor <adeyemovictoroyinlola@gmail.com>
2023-12-19 09:32:33 -0800

    all is well

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
  */
 
 void print_alphabet_x10(void)
 {
-	char chr;
+	char c;
 	int i = 0;
 
-	while (i <= 9)
-	{
-		for (chr = 'a'; chr <= 'z'; chr++)
+		while (i <= 9)
 		{
-			_putchar(chr);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
 		}
-
-		_putchar('\n');
-
-		i++;
-	}
+			_putchar('\n');
+			i++;
+		}
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ * _islower - entry point
+ * Description: function that checks for lowercase character
+ * @c:character to check if it islowercase
+ * Return: 0-if lowercase, 1-if not
  */
 
 int _islower(int c)
 {
-	if  (c >= 97 && c <= 122)
-		return (1);
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
 	else
-		return (0);
-
+	{
+	return (0);
+	}
 }
