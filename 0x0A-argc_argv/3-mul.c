28ef74df4a9265499cd81b16ccd3e1b35ccd5bc7
Adeyemo victor <adeyemovictoroyinlola@gmail.com>
2024-01-13 04:37:18 -0800

    balovic

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,19 +8,17 @@
 *
 * Return: 0
 */
-int main(int argc, char **argv)
-{
-int x, y;
+int main(int argc, char **argv){
+  int x, y;
+  
+  if (argc < 3){
+    printf("Error\n");
+    return (1);
+  }
 
-if (argc < 3)
-{
-printf("Error\n");
-return (1);
-}
-
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
+  x = atoi(argv[1]);
+  y = atoi(argv[2]);
+  printf("%d\n", x * y);
 
 return (0);
 }
