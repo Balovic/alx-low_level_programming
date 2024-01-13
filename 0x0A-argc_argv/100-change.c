b2e41866f9c44379ed11e1b054348804620514e9
Adeyemo victor <adeyemovictoroyinlola@gmail.com>
2024-01-13 04:43:04 -0800

    balovic

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,88 +1,39 @@
 #include <stdio.h>
-		
 #include <stdlib.h>
-		
-#include "main.h"
-		
 
-		
 /**
-		
- * main - prints the minimum number of coins to
-		
- * make change for an amount of money
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: 0 (Success), 1 (Error)
-		
- */
-		
-int main(int argc, char *argv[])
-		
+* main - adds positive numbers.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
 {
-		
-	int num, j, result;
-		
-	int coins[] = {25, 10, 5, 2, 1};
-		
+int i, n, sum = 0;
+char *flag;
 
-		
-	if (argc != 2)
-		
-	{
-		
-		printf("Error\n");
-		
-		return (1);
-		
-	}
-		
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
 
-		
-	num = atoi(argv[1]);
-		
-	result = 0;
-		
+for (i = 1; argv[i]; i++)
+{
+n = strtol(argv[i], &flag, 10);
+if (*flag)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+sum += n;
+}
+}
+printf("%d\n", sum);
 
-		
-	if (num < 0)
-		
-	{
-		
-		printf("0\n");
-		
-		return (0);
-		
-	}
-		
-
-		
-	for (j = 0; j < 5 && num >= 0; j++)
-		
-	{
-		
-		while (num >= coins[j])
-		
-		{
-		
-			result++;
-		
-			num -= coins[j];
-		
-		}
-		
-	}
-		
-
-		
-	printf("%d\n", result);
-		
-	return (0);
-		
+return (0);
 }
 
